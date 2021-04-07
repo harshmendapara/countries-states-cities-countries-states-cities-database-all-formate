e4b038005a13b0a75e53c77f5d645636369720b3
Harsh Mendapara <mendaparaharsh02@gmail.com>
2021-04-07 16:21:16 +0530

    first commit

---
+++ scripts/vendor/get_cities_by_state_country.php
@@ -0,0 +1,47 @@
+<?php
+require_once 'base.php';
+
+// Cities // 144153/10 = 14415.30
+// 980/apicalls/day/apikey
+
+$counter = "counter.txt";
+$f = fopen($counter, "r") or die("Unable to open file!");
+// $offset = (int)fgets($f); // 9801 - For Automated Fetch
+$offset = 1; // For Manual Fetch
+$limit = $offset + 9; // 9801 + 9 = 9810
+fclose($f);
+
+
+$countryWiki = 'PH';
+$stateFips = 'ABR';
+
+$file_name = "data/".$countryWiki.'_'.$stateFips.'.json';
+
+for ($i=0; $i<36; $i++): // 575 / 10
+    $url = "https://wft-geo-db.p.rapidapi.com/v1/geo/countries/".$countryWiki."/regions/".$stateFips."/cities?offset=".$offset."&limit=10";
+    echo $url.PHP_EOL;
+    $response = Unirest\Request::get($url,
+        array(
+            "X-RapidAPI-Host" => "wft-geo-db.p.rapidapi.com",
+            "X-RapidAPI-Key" => $API_KEY
+        )
+    );
+
+    $fp = fopen($file_name, 'a') or die("Unable to open file!");
+    fwrite($fp, json_encode((array)$response->body->data, JSON_UNESCAPED_UNICODE|JSON_PRETTY_PRINT|JSON_NUMERIC_CHECK).PHP_EOL);
+    fclose($fp);
+
+    if ($i%5 == 0): sleep($NUMBER_OF_SECONDS); endif;
+    $offset = $limit + 1; // 9810
+    $limit = $limit + 10; // 9820
+
+    $fps = fopen($counter, 'w');
+    fwrite($fps, $offset.PHP_EOL);
+    fclose($fps);
+
+    echo $offset.PHP_EOL;
+    echo $limit.PHP_EOL;
+
+    // print_r($response);
+    // print_r($response->body->data);
+endfor;
